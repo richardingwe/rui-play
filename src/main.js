be068a817c99b1979792762919dc2318d1ef8612
richardingwe <richardingwe5@gmail.com>
2021-08-01 11:03:51 +0100

    created a firebase project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount('#app')
+// styles
+import './assets/main.css';
+
+createApp(App).use(router).mount('#app');
