b0449dc5eae0804a92f5edef9db4aae4d7f1ca35
richardingwe <richardingwe5@gmail.com>
2021-08-01 17:11:27 +0100

    handling file upload

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -15,8 +15,8 @@
 			></textarea>
 			<!-- upload playlist image -->
 			<label>Upload Playlist Cover Image</label>
-			<input type="file" />
-			<div class="error"></div>
+			<input type="file" @change="handleChange" />
+			<div class="error">{{ fileError }}</div>
 
 			<button>Create</button>
 		</form>
@@ -29,11 +29,28 @@
 		setup() {
 			const title = ref('');
 			const description = ref('');
+			const file = ref(null);
+			const fileError = ref(null);
 			const handleSubmit = () => {
-				console.log(title.value, description.value);
+				if (file.value) {
+					console.log(title.value, description.value, file.value);
+				}
+			};
+			// allowed file types
+			const types = ['image/png', 'image/jpeg'];
+			const handleChange = (e) => {
+				let selected = e.target.files[0];
+				console.log(selected);
+				if (selected && types.includes(selected.type)) {
+					file.value = selected;
+					fileError.value = null;
+				} else {
+					file.value = null;
+					fileError.value = 'Please select an image file (png or jpg)';
+				}
 			};
 
-			return { title, description, handleSubmit };
+			return { title, description, handleSubmit, fileError, handleChange };
 		},
 	};
 </script>
