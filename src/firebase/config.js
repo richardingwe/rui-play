d5885157ebf262991f8473867fe4adcc73742a18
richardingwe <richardingwe5@gmail.com>
2021-08-01 11:21:24 +0100

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqkA5eVXjl57bJosfyh885EJ6ceCHzckA",
+    authDomain: "rui-play.firebaseapp.com",
+    projectId: "rui-play",
+    storageBucket: "rui-play.appspot.com",
+    messagingSenderId: "689098162058",
+    appId: "1:689098162058:web:d3a2e453f3c254449cd273"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
