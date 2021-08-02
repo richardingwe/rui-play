02dbd091d001fdfd410dc7141d7b9ca63933c7b8
richardingwe <richardingwe5@gmail.com>
2021-08-02 20:50:45 +0100

    created playlist ownership and delete button

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -9,6 +9,7 @@
 			<h2>{{ playlist.title }}</h2>
 			<p class="username">Created by {{ playlist.userName }}</p>
 			<p class="description">{{ playlist.description }}</p>
+			<button v-if="ownership">Delete Playlist</button>
 		</div>
 
 		<!-- song list -->
@@ -20,11 +21,21 @@
 
 <script>
 	import getDocument from '@/composables/getDocument';
+	import getUser from '@/composables/getUser';
+	import { computed } from 'vue';
 	export default {
 		props: ['id'],
 		setup(props) {
 			const { error, document: playlist } = getDocument('playlists', props.id);
-			return { error, playlist };
+			const { user } = getUser();
+			const ownership = computed(() => {
+				return (
+					playlist.value &&
+					user.value &&
+					user.value.uid == playlist.value.userId
+				);
+			});
+			return { error, playlist, ownership };
 		},
 	};
 </script>
